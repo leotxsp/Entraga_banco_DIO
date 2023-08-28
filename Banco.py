707e8b7e67a4f792956b3ccb9a222fb7c45a9975
leotxsp <leotxsp@gmail.com>
2023-08-28 20:58:43 -0300

    Entrega 01

--- Banco.py
+++ Banco.py
@@ -1,33 +1,67 @@
 menu = """
 
 [d] Depositar
+
 [s] Sacar
+
 [e] Extrato
-[s] Sair
+
+[q] Sair
 
 =>"""
 
-Saldo = 0
+saldo = 0
 limite = 500
-extrato = 0
+extrato = ""
 LIMITE_SAQUES = 3
-
+num_saques = 0
 while True:
 
     opcao = input(menu)
 
     if opcao == "d":
-        print("Deposito")
+        deposito = float(input("Digite o valor do depósito: \n"))
+        if deposito > 0:
+            saldo += deposito
+            extrato += f"Entrada: R${deposito:.2f}\n"
+        else:
+            print("Valor inválido!")
     
     elif opcao == "s":
-        print("Saque")
+        saque = float(input("Digite o valor do saque: "))
+
+        sem_saldo = saque > saldo
+
+        sem_limite = saque > limite
+
+        sem_limite_saques = num_saques == LIMITE_SAQUES
+
+        if sem_saldo:
+            print("Saldo insuficiente!")
+
+        elif sem_limite:
+            print("Voce atingiu o limite de R$500 por saque!")
+
+        elif sem_limite_saques:
+            print("Voce atingiu o limite de saques diários!")
+        
+        elif saque > 0:
+            saldo -= saque
+            extrato += f"Saida: R${saque:.2f}\n"
+            num_saques += 1
+        else:
+            print("Valor inválido!")
+
 
     elif opcao == "e":
-        print("Extrato")
-    
-    elif opcao == "s":
-        print("Sair")
+        print("\n<========================> Extrato <========================>\n")
+        print("Não há extrato disponível." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("\n<=========================================================>")
+
+    elif opcao == "q":
         break
+
     else:
         print("Opção inválida")
         continue
